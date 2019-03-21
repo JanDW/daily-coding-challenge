34d8c99af51d629afc62c90c2582aba1bcf0ad28
Jan De Wilde <jandw@mit.edu>
2019-03-21 10:59:07 -0400

    03-20-19 Account for 0

--- 03-20-2019.js
+++ 03-20-2019.js
@@ -4,5 +4,20 @@
 // Follow-up: what if you can't use division?
 
 const array1 = [1, 2, 3, 4, 5];
+let array2 = [];
 const totalProduct = array1.reduce((el,next) => el * next);
-array1.map(el => totalProduct / el);
+
+// Check to avoid division by 0
+// one multiplier === 0 => product === 0
+
+totalProduct === 0
+	? array2 = Array.from(Array(array1.length), () => 0)
+	: array2 = array1.map(el => totalProduct / el);
+
+console.log(array2);
+
+// Other solutions:
+// https://galaiko.rocks/posts/2018-07-03/
+// https://stackoverflow.com/questions/2680548/given-an-array-of-numbers-return-array-of-products-of-all-other-numbers-no-div
+//
+// @TODO This still isn't quite right, for the 0 position in array1, the product isn't 0 (unless there is a second 0 in the array).
