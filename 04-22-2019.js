6462ea949c874d80a3b69e9d13e4eae17bd3daf5
Jan De Wilde <jandw@mit.edu>
2019-04-23 10:07:58 -0400

    Minor changes to 04-22-2019

--- 04-22-2019.js
+++ 04-22-2019.js
@@ -1,4 +1,5 @@
-// Compute the running median of a sequence of numbers.
+// Problem: Compute the running median of a sequence of numbers.
+//
 // That is, given a stream of numbers, print out the median of the
 // list so far on each new element.
 // Recall that the median of an even-numbered list is
@@ -11,14 +12,26 @@
 // [2, 1.5, 2, 3.5, 2, 2, 2]
 
 
+// Use Babel with Quokka.js IDE plugin
+{
+  babel: {
+    presets: ["es2015"]
+  }
+}
 
-
+// After some googling:
 // Most optimal solution is to use heaps: https://www.youtube.com/watch?v=1CxyVdA_654
 
 // Collections.js provides a heap datastructure constructor
 // http://www.collectionsjs.com/heap
+//
+// npm install collections --save
+
 const Heap = require("collections/heap");
-const sourceArray = [2, 1, 5, 7, 2, 0, 5];
+//const sourceArray = [2, 1, 5, 7, 2, 0, 5];
+
+// Bigger array to test performance
+const sourceArray = Array(10000).fill().map(() => Math.round(Math.random() * 1000));
 
 
 // This function serves to balance two heaps, so they
@@ -41,46 +54,47 @@ const medianArray = arr => {
   const maxHeap = new Heap();
 
   return arr.map(element => {
-    // Deal with first element
+    // Deal with first element, push to maxHeap
     if (maxHeap.length === 0) {
       maxHeap.push(element);
-      return element; //?
+      return element;
     }
 
     // Deal with second element
+    // After this, both Heaps will contain an element
     if (maxHeap.length === 1 && minHeap.length === 0) {
       if (element >= maxHeap.peek()) {
         minHeap.push(element);
       } else {
-        // move to minHeap
+        // element is smaller than prev element
+        // so move prev el to minHeap and push current el to maxHeap
         minHeap.push(maxHeap.pop());
         maxHeap.push(element);
       }
-      return (maxHeap.peek() + minHeap.peek()) / 2; //?
+      return (maxHeap.peek() + minHeap.peek()) / 2;
     }
 
-    // Now heaps are initialized see which one the element should go in:
-    // if the element is smaller than the root of the minHeap, push it
-
+    // Now heaps are initialized see which one the element should go in
     if (element <= maxHeap.peek()) {
-      maxHeap.push(element); //?
+      maxHeap.push(element);
     } else {
-      minHeap.push(element); //?
+      minHeap.push(element);
     }
 
-    // Balance heaps
+    // Balance heaps, so their lengths are same or +1
     balanceHeaps(maxHeap, minHeap);
 
     // Return average of heap roots if even number of elements
     if (maxHeap.length === minHeap.length) {
-      return (maxHeap.peek() + minHeap.peek()) / 2; //?
+      return (maxHeap.peek() + minHeap.peek()) / 2;
     } else if (maxHeap.length > minHeap.length) {
-      return maxHeap.peek(); //?
+      return maxHeap.peek();
     } else {
-      return minHeap.peek(); //?
+      return minHeap.peek();
     }
   });
 };
 
-medianArray(sourceArray); //?
+const result = medianArray(sourceArray); //?. $
+console.log(result.length);
 // Should be: 2, 1.5, 2, 3.5, 2, 2, 2
